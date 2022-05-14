0a3427e1a5d910cbe4d4389eb93a884c557f4fbe
Asia <asia.godula@gmail.com>
2022-05-14 11:17:47 +0200

    Working on components

--- moz-todo-react/src/index.js
+++ moz-todo-react/src/index.js
@@ -4,12 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const DATA = [
+    { id: "todo-0", name: "Eat", completed: true },
+    { id: "todo-1", name: "Sleep", completed: false },
+    { id: "todo-2", name: "Repeat", completed: false }
+];
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+root.render( <React.StrictMode>
     <App />
-  </React.StrictMode>
+    </React.StrictMode>
 );
+
+ReactDOM.render(<App tasks={DATA} />,
+    document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
